607fc9fefbb7bc670ce59e636da3d33e925cd53a
Fauzi <fauzi_farhat@hotmail.com>
2018-08-16 16:00:44 -0300

    Comentario Qualquer soh pra zuar

--- src/main/java/br/com/db1/App.java
+++ src/main/java/br/com/db1/App.java
@@ -1,13 +1,15 @@
 package br.com.db1;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        int zero = 0;
+        Integer zeroObjeto = 0;
+        zeroObjeto.doubleValue();
+        
+        char vogal = 'a';
+        double umPontoDois = 1.2;
     }
 }
---
+++ src/main/java/br/com/db1/ExercicioDeColecao.java
@@ -0,0 +1,15 @@
+package br.com.db1;
+
+import java.util.ArrayList;
+
+public class ExercicioDeColecao {
+	
+	public static void main(String[] args) {
+		ArrayList<String> nomes = new ArrayList<String>();
+		exercicio2();
+	}
+
+	public static Integer exercicio2(List<String> nomes) {
+		return nomes.size();
+	}
+}
