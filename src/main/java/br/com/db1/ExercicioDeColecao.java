4f40840732bca763db6a3f89e2e54ea29723b60d
Fauzi <fauzi_farhat@hotmail.com>
2018-08-16 18:27:18 -0300

    Exercicios Collections

--- src/main/java/br/com/db1/ExercicioDeColecao.java
+++ src/main/java/br/com/db1/ExercicioDeColecao.java
@@ -1,15 +1,64 @@
 package br.com.db1;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class ExercicioDeColecao {
-	
-	public static void main(String[] args) {
-		ArrayList<String> nomes = new ArrayList<String>();
-		exercicio2();
-	}
 
-	public static Integer exercicio2(List<String> nomes) {
+	public static void main(String[] args) {
+		exercicio1();
+		exercicio2();
+		exercicio3();
+		exercicio4();
+		exercicio5();
+	}
+	
+	public static ArrayList<String> exercicio1(){
+		ArrayList<String> cores = new ArrayList<String>();
+		cores.add("Laranja");
+		cores.add("Azul");
+		return cores;
+	}
+	
+	public static Integer exercicio2() {
+		ArrayList<String> nomes = new ArrayList<String>();
+		nomes.add("Fauzi");
+		nomes.add("Fauzao");
 		return nomes.size();
 	}
+	
+	public static List<String> exercicio3(){
+		List<String> parentes = new ArrayList<String>();
+		parentes.add("Pai");
+		parentes.add("Mãe");
+		parentes.add("Irmão");
+		parentes.add("Irmã");
+		parentes.add("Tio");
+		parentes.add("Tia");
+		parentes.add("Primo");
+		parentes.remove("Pai");
+		parentes.remove("Mãe");
+		return parentes;
+	}
+	
+	public static List<String> exercicio4(){
+		List<String> cidades = new ArrayList<String>();
+		cidades.add("Campo Grande");
+		cidades.add("Nova Andradina");
+		cidades.add("Batayporã");
+		cidades.remove(1);
+		return cidades;
+	}
+	
+	public static List<String> exercicio5(){
+		List<String> cores = new ArrayList<String>();
+		cores = exercicio1();
+		System.out.println(cores);
+		Collections.sort(cores);
+		System.out.println(cores);
+		return cores	;
+	}
+	
+	
 }
--- src/test/java/br/com/db1/ExercicioDeColecaoTest.java
+++ src/test/java/br/com/db1/ExercicioDeColecaoTest.java
@@ -7,8 +7,34 @@ import org.junit.Test;
 public class ExercicioDeColecaoTest {
 
 	@Test
+	public void exercicio1Test() {
+		ExercicioDeColecao ex = new ExercicioDeColecao();
+		assertTrue(ex.exercicio1().get(0).equals("Azul"));
+		assertTrue(ex.exercicio1().get(1).equals("Laranja"));
+	}
+	
+	@Test
 	public void exercicio2Test() {
 		ExercicioDeColecao ex = new ExercicioDeColecao();
 		assertTrue(ex.exercicio2() == 2);
+	}
+	
+	@Test
+	public void exercicio3Test() {
+		ExercicioDeColecao ex = new ExercicioDeColecao();
+		assertTrue(ex.exercicio3().get(0) != "Pai");
+		assertTrue(ex.exercicio3().get(1) != "Mãe");
+	}
+	
+	@Test
+	public void exercicio4Test() {
+		ExercicioDeColecao ex = new ExercicioDeColecao();
+		assertTrue(ex.exercicio4().get(1) != "Nova Andradina");
+	}
+	
+	@Test
+	public void exercicio5Test() {
+		ExercicioDeColecao ex = new ExercicioDeColecao();
+
 	}
 }
